2a93adc3c3d3a17df15e5feee2941dfec0a1826d
Arina Kazakova <arinakazakova343@gmail.com>
2023-08-17 20:46:11 +0400

    Hello title

--- src/WelcomeWindow.java
+++ src/WelcomeWindow.java
@@ -8,7 +8,7 @@ public class WelcomeWindow extends JFrame {
     JButton setContinueButton;
 
     public WelcomeWindow() {
-        super("Список задач");
+        super("Hello");
         setSize(1000, 400);
         setLocation(5, 5);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
